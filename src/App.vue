f59cf278646069ad04b1534b8fac08399f79e9be
aleasper <alexidest@gmail.com>
2020-11-21 17:10:37 +0300

    Remove HelloWorld and add info into main page of app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <MainPage></MainPage>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+//import HelloWorld from './components/HelloWorld.vue'
+
+import MainPage from "@/components/MainPage";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainPage
   }
 }
 </script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="org-label-text">
+    <p>Aleasper NPO</p>
+  </div>
+  <div>
+    <p>Страница находится в разработке</p>
+    <p>https://github.com/aleasper/AutomationViewerFrontend</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage"
+}
+</script>
+
+<style scoped>
+.org-label-text{
+  font-size: 22px;
+}
+</style>

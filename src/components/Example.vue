d39583ae5264146546f81741a15db8fdc3b16fa8
aleasper <alexidest@gmail.com>
2020-12-09 22:30:38 +0300

    Update test post request to backend

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -2,12 +2,31 @@
   <div class="org-label-text">
     <p>Aleasper NPO</p>
     <h1>Введите текст</h1>
-    <input type = "text" v-model = "mess">
-    <button @click="sendData">Отправить текст в консоль бразуера</button>
+    <input type="text" v-model="mess">
+    <button @click="printVKData">Отправить текст в консоль бразуера</button>
   </div>
 </template>
 
 <script>
+//TODO Вынести в отдельный файл
+/* Вне Vue компонента, т.к. это отдельная функция не имеющая отношения к компоненту*/
+async function sendData(url, data) {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors', // no-cors, *cors, same-origin
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  if (response.ok){
+    return await response.json();
+  } else {
+    console.error(`Fetch err to ${url} with: ${response.status}`);
+    return await null;
+  }
+}
+
 export default {
   data() {
     return {
@@ -15,19 +34,19 @@ export default {
     }
   },
   methods: {
-    async sendData() {
-      const response = await fetch('https://cors-anywhere.herokuapp.com/https://automation-viewer-backend.herokuapp.com/vk_data', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          app_id: this.mess
-        })
+    printVKData(){
+      let url = 'http://127.0.0.1:8000/vk_data';
+      let data = {
+        'app_id': this.mess
+      };
+      sendData(url, data).then( res => {
+        console.log(res);
+      }).catch(er => {
+        console.error(er);
       })
-      console.log(response)
+
     }
-  }
+  },
 }
 </script>
 

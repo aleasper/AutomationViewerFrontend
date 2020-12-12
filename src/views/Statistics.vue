f0574b5656b0907d8709305da5fc8c4c69c294a2
aleasper <alexidest@gmail.com>
2020-12-12 20:33:37 +0300

    Add displaying stat data from backend

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,16 +1,83 @@
 <template>
-  <div>
-    <!--
-    <UploadingData></UploadingData>
-    -->
-
+  <div class="statistics-div">
+    <v-row class="input-data-div">
+      <v-col
+          md="2"
+      >
+        <v-text-field
+            class="app-id-input"
+            v-model="appId"
+            outlined
+            label="Application id"
+        >
+        </v-text-field>
+      </v-col>
+      <v-col
+          md="1"
+      >
+        <v-btn
+            class="get-app-data-btn"
+            v-on:click="getStat"
+        >Get statistics</v-btn>
+      </v-col>
+    </v-row>
+    <div class="display-data-div">
+      <v-data-table
+          :headers="tableHeaders"
+          :items="tableItems"
+          :items-per-page="10"
+          class="elevation-1"
+          multi-sort
+      ></v-data-table>
+    </div>
   </div>
 </template>
 
 <script>
+import {sendData} from '@/functions.js'
 
 export default {
-  name: "SecondPage",
+  name: "Statistics",
+  data: () => ({
+    appId: '',
+    tableHeaders: [
+      {
+        text: 'Resource',
+        value: 'name'
+      },
+      {
+        text: 'Likes',
+        value: 'likes'
+      },
+      {
+        text: 'Comments',
+        value: 'comments'
+      },
+      {
+        text: 'Reposts',
+        value: 'reposts'
+      }
+    ],
+    tableItems: [],
+  }),
+  methods:{
+    getStat(){
+      let url = 'https://automation-viewer-backend.herokuapp.com/vk_data';
+      let data = {
+        'app_id': this.appId
+      };
+      sendData(url, data).then( res => {
+        console.log(res);
+        if (res !== []){
+          for (let i in res['app_info']){
+            this.tableItems.push(res['app_info'][i]['info']);
+          }
+        }
+      }).catch(er => {
+        console.error(er);
+      });
+    }
+  },
   components: {
   }
 }
@@ -18,4 +85,32 @@ export default {
 
 <style scoped>
 
+.statistics-div{
+  margin: 4px 0;
+  width: 100%;
+  height: calc(100% - 8px);
+}
+
+.input-data-div{
+  width: 100%;
+  min-height: 110px;
+  max-height: 160px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: baseline;
+}
+
+.display-data-div{
+  width: 100%;
+  height: max-content;
+}
+
+.app-id-input{
+
+}
+.get-app-data-btn{
+
+}
 </style>
